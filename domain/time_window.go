a9edce9fd415acb1e417b4ac40423c95964e164b
taiyoh <sun.basix@gmail.com>
2019-07-25 08:07:38 +0900

    add TimeWindow model

---
+++ domain/time_window.go
@@ -0,0 +1,74 @@
+package domain
+
+import "time"
+
+// TimeWindow provides price segment by supplied date.
+type TimeWindow struct {
+	t       time.Time
+	holiday bool
+}
+
+// TimeWindowType represents segment types for price by time.
+type TimeWindowType int
+
+const (
+	// WeekDayDayTime represents TimeWindowType which explains weekday and daytime.
+	WeekDayDayTime TimeWindowType = iota + 1
+	// WeekDayNightTime represents TimeWindowType which explains weekday and night-time.
+	WeekDayNightTime
+	// HolidayDayTime represents TimeWindowType which explains holiday and daytime.
+	HolidayDayTime
+	// HolidayNightTime represents TimeWindowType which explains holiday and night-time.
+	HolidayNightTime
+	// MovieDay represents TimeWindowType which explain the first day of this month.
+	MovieDay
+)
+
+type dayType int
+
+const (
+	weekday dayType = iota + 1
+	holiday
+)
+
+type hourType int
+
+const (
+	daylight hourType = iota + 1
+	lateshow
+)
+
+var typeByDayAndHour = map[dayType]map[hourType]TimeWindowType{
+	weekday: map[hourType]TimeWindowType{
+		daylight: WeekDayDayTime,
+		lateshow: WeekDayNightTime,
+	},
+	holiday: map[hourType]TimeWindowType{
+		daylight: HolidayDayTime,
+		lateshow: HolidayNightTime,
+	},
+}
+
+func (w TimeWindow) dayType() dayType {
+	wd := w.t.Weekday()
+	if w.holiday || wd == time.Saturday || wd == time.Sunday {
+		return holiday
+	}
+	return weekday
+}
+
+func (w TimeWindow) hourType() hourType {
+	if w.t.Hour() < 20 {
+		return daylight
+	}
+	return lateshow
+}
+
+// Type calculates TimeWindowType by supplied time.
+func (w TimeWindow) Type() TimeWindowType {
+	if w.t.Day() == 1 {
+		return MovieDay
+	}
+	t, _ := typeByDayAndHour[w.dayType()][w.hourType()]
+	return t
+}
